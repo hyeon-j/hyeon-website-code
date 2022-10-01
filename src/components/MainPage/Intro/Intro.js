353c7d03ce42367fbe0732258142969b668e1213
hyeon-j <47257622+hyeon-j@users.noreply.github.com>
2022-09-30 23:54:21 -0400

    Created and added Intro Component.
    Not much done on this commit.

---
+++ src/components/MainPage/Intro/Intro.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Intro.css'
+
+export default function Intro() {
+  return (
+    <>
+        <div class='intro__container'>
+            <div className='intro__text'>
+                Hello, I am Hyeon, a Web Developer
+            </div>
+            <div className='intro__picture'>
+                Temporary Spot for IMAGE
+            </div>
+        </div>
+
+
+
+
+    </>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Intro from '../components/MainPage/Intro/Intro.js';
+
 export default function main() {
   return (
     <>
+
+      <h1>MAINasdadasdasd</h1>
+      <Intro />
+
       <h1>MAIN</h1>
+
+
+
+
+
+
       <Link to='/sort'>to sort</Link>
 
       <br></br>
